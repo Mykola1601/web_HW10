07a79768587b59c67082a8b7b8d59063d027f047
Mykola1601 <nik160186@gmail.com>
2024-02-06 21:56:03 +0200

    paginator fix

--- homework/quotes/templatetags/extract.py
+++ homework/quotes/templatetags/extract.py
@@ -1,7 +1,6 @@
-from ..utils import get_mongodb
-from django.shortcuts import render, get_object_or_404
 
-from bson.objectid import ObjectId
+# from bson.objectid import ObjectId
+from django.shortcuts import  get_object_or_404
 from django import template
 from ..models import Author, Tag
 
@@ -9,25 +8,16 @@ from ..models import Author, Tag
 register = template.Library()
 
 
-# def get_author(id_):
-#     db = get_mongodb()
-#     author = db.author.find_one({"_id":ObjectId(id_)  })
-#     # author = Author.objects.all({"_id":ObjectId(id_)  })
-#     return author["fullname"]
-
 def get_author(id_):
-    print("........",id_)
+    # print("........",id_)
     author = get_object_or_404(Author, id = id_)
-    print("...........", author.fullname)
+    # print("...........", author.fullname)
     return author.fullname
 
 
 
 def get_tags(id_):
-    # print("...tag id ......",id_)
     tags = Tag.objects.filter(quote = id_).all()
-    # for t in tags:
-    #     print("...........", t.name)
     return tags
 
 
--- homework/quotes/views.py
+++ homework/quotes/views.py
@@ -1,17 +1,18 @@
-# from .utils import get_mongodb
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import Quote
 
 
 def main(request, page=1):
-    # db = get_mongodb()          # for mongo
-    # quotes = db.quote.find()    # for mongo
     quotes = Quote.objects.all()
     per_page = 10
     paginator = Paginator(list(quotes), per_page)
     quotes_on_page = paginator.page(page)
     return render(request, "quotes/index.html", context = {"quotes" : quotes_on_page} )
+
+
+
+
 
 
 
